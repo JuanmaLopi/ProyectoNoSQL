2e673d24e2ed8f40fa2475a86e74e9b5c174546c
JuanmaLopi <124200475+JuanmaLopi@users.noreply.github.com>
2024-05-29 08:56:29 -0500

    Add files via upload

---
+++ Exception/RecursoNoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.apiweb.backend.Exception;
+
+public class RecursoNoEncontradoException extends RuntimeException{
+    public RecursoNoEncontradoException(String mensaje){
+        super(mensaje);
+    }
+    
+}
